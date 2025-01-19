81875d23088c48593658e34ebf20f3f126a0ced8
Rigidity <me@rigidnetwork.com>
2025-01-19 12:28:53 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import banner from "/banner.png";
+
+function App() {
+  return (
+    <>
+      <h1>Fancy Fauna</h1>
+      <img src={banner} style={{ borderRadius: "10px" }} />
+    </>
+  );
+}
+
+export default App;
