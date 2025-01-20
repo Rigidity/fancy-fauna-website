b6e1ecd36694d2b4904bedda31d08d87ec8fc37c
Rigidity <me@rigidnetwork.com>
2025-01-20 07:46:07 -0500

    Update site and use tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,20 @@ function App() {
   return (
     <>
       <h1>Fancy Fauna</h1>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+      <a
+        className="flex flex-col md:flex-row items-center justify-center gap-4 p-4 shadow-xl rounded-lg bg-sky-300 hover:bg-sky-400 transition-colors my-4"
+        href="https://mintgarden.io/collections/fancy-fauna-col1f90cpk9msmsf9zmecqcr8h0xu40jyr66j5d5aj336qkf50knxc3srq2d6j"
+        target="_blank"
       >
-        <a
-          style={{
-            color: "black",
-            fontSize: "2rem",
-            display: "inline-flex",
-            alignItems: "center",
-            padding: "20px",
-            gap: "6px",
-            borderRadius: "20px",
-            boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
-            marginBottom: "20px",
-          }}
-          href="https://mintgarden.io/collections/fancy-fauna-col1f90cpk9msmsf9zmecqcr8h0xu40jyr66j5d5aj336qkf50knxc3srq2d6j"
-          target="_blank"
-        >
-          <img
-            src="https://mintgarden.io/mint-logo.svg"
-            style={{ width: "30px", height: "30px" }}
-            alt="MintGarden logo"
-          />
+        <img
+          src="https://mintgarden.io/mintgarden-logo.svg"
+          alt="MintGarden logo"
+          className="h-8 md:h-10"
+        />
+        <div className="text-gray-950 text-xl md:text-2xl">
           Click below to mint
-        </a>
-      </div>
+        </div>
+      </a>
       <a
         href="https://mintgarden.io/collections/fancy-fauna-col1f90cpk9msmsf9zmecqcr8h0xu40jyr66j5d5aj336qkf50knxc3srq2d6j"
         target="_blank"
