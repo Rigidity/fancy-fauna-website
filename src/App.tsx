19889010f86aae2fd1036090520470f8fff1fe8b
Rigidity <me@rigidnetwork.com>
2025-01-20 07:30:43 -0500

    Update site

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,49 @@ function App() {
   return (
     <>
       <h1>Fancy Fauna</h1>
-      <img src={banner} style={{ borderRadius: "10px" }} />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <a
+          style={{
+            color: "black",
+            fontSize: "2rem",
+            display: "inline-flex",
+            alignItems: "center",
+            padding: "20px",
+            gap: "6px",
+            borderRadius: "20px",
+            boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
+            marginBottom: "20px",
+          }}
+          href="https://mintgarden.io/collections/fancy-fauna-col1f90cpk9msmsf9zmecqcr8h0xu40jyr66j5d5aj336qkf50knxc3srq2d6j"
+          target="_blank"
+        >
+          <img
+            src="https://mintgarden.io/mint-logo.svg"
+            style={{ width: "30px", height: "30px" }}
+            alt="MintGarden logo"
+          />
+          Click below to mint
+        </a>
+      </div>
+      <a
+        href="https://mintgarden.io/collections/fancy-fauna-col1f90cpk9msmsf9zmecqcr8h0xu40jyr66j5d5aj336qkf50knxc3srq2d6j"
+        target="_blank"
+      >
+        <img
+          src={banner}
+          style={{
+            borderRadius: "10px",
+            boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
+            maxWidth: "100%",
+          }}
+        />
+      </a>
     </>
   );
 }
